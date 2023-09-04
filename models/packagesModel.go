a5c4eac6e489118936e603266cf51898b202e5c2
tzsombi01 <toreky.zsombor@gmail.com>
2023-09-04 20:28:00 +0200

    feature(packages-model): Made changes to package model

--- models/packagesModel.go
+++ models/packagesModel.go
@@ -5,12 +5,15 @@ import "gorm.io/gorm"
 // Package DB table model
 type Package struct {
 	gorm.Model
-	UserID             uint    // Sender's id
-	DestinationAddress string  // Destination address (street + house number)
-	Content            string  // What is inside the package
-	Price              float64 // Delivery fee
-	Note               string  // Extra note
-	CourierID          uint    // Courier's id who delivers the package
+	Id                  uint    // Entity id
+	UserID              uint    // Sender's id
+	ReceiverId          uint    // Receiver's id
+	DestinationLockerId uint    // Destination locker's id
+	Size                string  // Small, Medium, Large, needs to be an enum
+	Price               float64 // Delivery fee
+	Code                string  // Code to open the locker
+	Note                string  // Extra note
+	CourierID           uint    // Courier's id who delivers the package
 }
 
 // Name of the Package structs in the DB
