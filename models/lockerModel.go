71262c42029392aaf16a4e60c0db41d24e83b343
Szilágyi Dominik <dominik_szilagyi@yahoo.com>
2023-09-16 08:12:03 +0200

    feat(locker-cap): Added capacity property to the lockers

--- models/lockerModel.go
+++ models/lockerModel.go
@@ -13,6 +13,9 @@ type Locker struct {
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 	Address   string         // Address of the locker
+	/* Possibly there will be 2 more column for X and Y geo. points if we want to show them on a map */
+
+	Capacity uint // Shows how many packages could be there
 }
 
 // Name of the Locker structs in the DB
