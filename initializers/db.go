e490cf60171022d1f34fab4aac962b6ff542d7b0
Károly Szakály <karoly.szakaly2000@gmail.com>
2023-10-25 17:15:54 +0200

    refactor(db): create connection string from existing env vars

--- initializers/db.go
+++ initializers/db.go
@@ -19,7 +19,7 @@ var DB *gorm.DB
 // Connectiing to the database based on the environment variables
 func ConnectToDatabase() {
 	var err error
-	dsn := os.Getenv("DB_URL")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), 5432)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
